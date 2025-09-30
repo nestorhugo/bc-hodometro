9d2e73927197f9dda7532c96d7315238e397ba1d
nestorhugo <nestorhugo@proton.me>
2025-09-30 07:39:30 -0300

    feat: refactor vehicleTrackerStore to use reactive filterData inside store

--- src/stores/vehicleTrackerStore.ts
+++ src/stores/vehicleTrackerStore.ts
@@ -1,36 +1,52 @@
 import apiCall, { type ApiResponse } from "@/functions/apiCall";
+import {
+  setDateToYesterday,
+  setEndOfDay,
+  setStartOfDay,
+} from "@/functions/utils";
+import type { FilterData } from "@/types/Filter";
 import type { Vehicle } from "@/types/Vehicle";
 import { defineStore } from "pinia";
 
 export const useVehicleTrackerStore = defineStore("vehicle", () => {
   const vehiclesData = ref<ApiResponse<Vehicle>>();
 
-  function getVehiclesData(payload: {
-    startDate: string;
-    endDate: string;
-    idTms?: string[];
-    licensePlate?: string[];
-    divisionId?: number[];
-    rows: number;
-    page: number;
-  }) {
+  const filterData = reactive<FilterData>({
+    startDate: setDateToYesterday(new Date()),
+    endDate: new Date(),
+    idTms: [],
+    licensePlates: [],
+    divisionIds: [],
+    pagination: {
+      itemsPerPage: 10,
+      totalPages: 0,
+      pageActive: 1,
+      totalItems: 0,
+    },
+  });
+
+  function getVehiclesData() {
     return apiCall<Vehicle>({
       method: "get",
       path: "/Vehicles/TrackerOdometer",
       queries: {
-        StartDate: payload.startDate,
-        EndDate: payload.endDate,
-        IdTms: payload.idTms,
-        LicensePlate: payload.licensePlate,
-        DivisionId: payload.divisionId,
-        Rows: payload.rows,
-        Page: payload.page,
+        StartDate: setStartOfDay(filterData.startDate),
+        EndDate: setEndOfDay(filterData.endDate),
+        IdTms: filterData.idTms,
+        LicensePlate: filterData.licensePlates,
+        DivisionId: filterData.divisionIds,
+        Rows: filterData.pagination.itemsPerPage,
+        Page: filterData.pagination.pageActive,
       },
     }).then((res) => {
       vehiclesData.value = res;
+
+      filterData.pagination.totalItems = res.totalItems;
+      filterData.pagination.totalPages = res.totalPages;
+      filterData.pagination.pageActive = res.pageActive;
       return res;
     });
   }
 
-  return { vehiclesData, getVehiclesData };
+  return { vehiclesData, filterData, getVehiclesData };
 });
